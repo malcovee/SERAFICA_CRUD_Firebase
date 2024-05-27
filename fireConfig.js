8fafc852d4aab718f0e03e63b0f208d2ac6c8e17
malcovee <144977672+malcovee@users.noreply.github.com>
2024-05-27 15:37:48 +0800

    initial commit

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAfwd7htKlzNUIO9msKgmU1f9DeVJJ7JoQ",
+  authDomain: "crud2-d32c4.firebaseapp.com",
+  databaseURL: "https://crud2-d32c4-default-rtdb.firebaseio.com",
+  projectId: "crud2-d32c4",
+  storageBucket: "crud2-d32c4.appspot.com",
+  messagingSenderId: "1059787362344",
+  appId: "1:1059787362344:web:8e4d139a13ec372e88651d"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
